e5fe6ec3ec8e502bb8bf037e90326d1443883c00
EmanuelPedrini <mistydorydory@gmail.com>
2026-03-06 22:25:55 -0300

    Add files via upload

--- testeaula01.py
+++ testeaula01.py
@@ -1,8 +1,11 @@
+#INPUT
 TdeVic = int(input("Insira seu Número de Vitórias"))
 TdeLos = int(input("Insira seu Número de Derrotas"))
+
+#CALCULA A PORCENTAGEM DE TG
 Tg = int((TdeVic) + (TdeLos))
 VicPercent = (TdeVic * 100 / Tg)
-Tg = str(Tg)
-VicPercent = str(VicPercent)
-print("Você jogou " + Tg + " Partidas")
-print("Ganhando " +)
+
+#MOSTRA OS RESULTADOS AO USUÁRIO
+print("Você jogou " + str(Tg) + " Partidas")
+print("Ganhando " + str(VicPercent) + "% das Partidas")

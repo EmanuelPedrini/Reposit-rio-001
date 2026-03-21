dad59b1190ea0ae59b3e4b125e37386f7752db4f
EmanuelPedrini <mistydorydory@gmail.com>
2026-03-20 22:18:04 -0300

    Add files via upload

--- retroquest.py
+++ retroquest.py
@@ -1,12 +1,18 @@
-import random
-import sys
+import random; import sys
+gamerunning=2
+
 start=input("Wanna to start retroquest? yep or exit  ")
 if start=="yep":
     gamerunning=1
     print("ok")
 elif start=="exit":
-    sys.exit()
-
+    gamerunning=0
 def rolld20():
     return random.randint(1,20)
 
+personagem={
+    "name" : "guerreiro",
+    "acthealth" : 3,
+    "maxhealth": 3
+    }
+print(personagem["name"])
